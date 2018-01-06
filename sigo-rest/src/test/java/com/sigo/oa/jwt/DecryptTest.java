ebfffb86a8e7047267a0fe1bf492111701e1d3dd
guochang <guochang@sigo.cn>
2018-01-06 20:24:30 +0800

    项目基础环境搭建 郭昶

---
+++ sigo-rest/src/test/java/com/sigo/oa/jwt/DecryptTest.java
@@ -0,0 +1,44 @@
+package com.sigo.oa.jwt;
+
+import com.alibaba.fastjson.JSON;
+import com.sigo.oa.core.util.MD5Util;
+import com.sigo.oa.rest.common.SimpleObject;
+import com.sigo.oa.rest.modular.auth.converter.BaseTransferEntity;
+import com.sigo.oa.rest.modular.auth.security.impl.Base64SecurityAction;
+
+/**
+ * jwt测试
+ *
+ * @author guochang
+ * @date 2017-08-21 16:34
+ */
+public class DecryptTest {
+
+    public static void main(String[] args) {
+
+        String key = "mySecret";
+
+        String compactJws = "eyJhbGciOiJIUzUxMiJ9.eyJyYW5kb21LZXkiOiJxczV4ZjciLCJzdWIiOiJhZG1pbiIsImV4cCI6MTUwNjM0Mzk4NywiaWF0IjoxNTA1NzM5MTg3fQ.N5_npknF-w_pq_3bi-cRp0HkjQqOVlK_dTh5QPIDYcWYCujp4uQ5-QrHDB86azHhsNKVgwpvh1_0ZkxmmEFsEQ";
+        String salt = "qs5xf7";
+
+        SimpleObject simpleObject = new SimpleObject();
+        simpleObject.setUser("guochang");
+        simpleObject.setAge(12);
+        simpleObject.setName("ffff");
+        simpleObject.setTips("code");
+
+        String jsonString = JSON.toJSONString(simpleObject);
+        String encode = new Base64SecurityAction().doAction(jsonString);
+        String md5 = MD5Util.encrypt(encode + salt);
+
+        BaseTransferEntity baseTransferEntity = new BaseTransferEntity();
+        baseTransferEntity.setObject(encode);
+        baseTransferEntity.setSign(md5);
+
+        System.out.println(JSON.toJSONString(baseTransferEntity));
+
+        //System.out.println("body = " + Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getBody());
+        //System.out.println("header = " + Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getHeader());
+        //System.out.println("signature = " + Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getSignature());
+    }
+}
