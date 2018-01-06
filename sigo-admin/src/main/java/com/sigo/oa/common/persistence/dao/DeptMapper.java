4a85630abed9e8ebe554fbf22986486427bf1fec
guochang <guochang@sigo.cn>
2018-01-06 20:25:08 +0800

    项目基础环境搭建 郭昶

---
+++ sigo-admin/src/main/java/com/sigo/oa/common/persistence/dao/DeptMapper.java
@@ -0,0 +1,16 @@
+package com.sigo.oa.common.persistence.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.sigo.oa.common.persistence.model.Dept;
+
+/**
+ * <p>
+  * 部门表 Mapper 接口
+ * </p>
+ *
+ * @author guochang
+ * @since 2017-07-11
+ */
+public interface DeptMapper extends BaseMapper<Dept> {
+
+}
