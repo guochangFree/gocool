6a66ab48756b83c296728fdf990128136152026e
guochang <guochang@sigo.cn>
2018-01-06 20:22:04 +0800

    项目基础环境搭建 郭昶

---
+++ sigo-admin/src/main/java/com/sigo/oa/common/constant/cache/Cache.java
@@ -0,0 +1,15 @@
+package com.sigo.oa.common.constant.cache;
+
+/**
+ * 所有缓存名称的集合
+ *
+ * @author guochang
+ * @date 2017-04-24 21:56
+ */
+public interface Cache {
+
+    /**
+     * 常量缓存
+     */
+    String CONSTANT = "CONSTANT";
+}
---
+++ sigo-admin/src/main/java/com/sigo/oa/common/exception/BussinessException.java
@@ -0,0 +1,15 @@
+package com.sigo.oa.common.exception;
+
+import com.sigo.oa.core.exception.GunsException;
+
+/**
+ * @author guochang
+ * @Description 业务异常的封装
+ * @date 2016年11月12日 下午5:05:10
+ */
+public class BussinessException extends GunsException {
+
+	public BussinessException(BizExceptionEnum bizExceptionEnum) {
+		super(bizExceptionEnum.getCode(), bizExceptionEnum.getMessage(), bizExceptionEnum.getUrlPath());
+	}
+}
