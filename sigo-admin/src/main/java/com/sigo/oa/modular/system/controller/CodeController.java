aac3858c999c51568d79b15e1ba8ae93a17cce5a
guochang <guochang@sigo.cn>
2018-01-06 20:23:13 +0800

    项目基础环境搭建 郭昶

---
+++ sigo-admin/src/main/java/com/sigo/oa/modular/system/controller/CodeController.java
@@ -0,0 +1,68 @@
+package com.sigo.oa.modular.system.controller;
+
+import com.sigo.oa.common.exception.BussinessException;
+import com.sigo.oa.core.template.config.ContextConfig;
+import com.sigo.oa.core.template.engine.SimpleTemplateEngine;
+import com.sigo.oa.common.annotion.Permission;
+import com.sigo.oa.common.constant.Const;
+import com.sigo.oa.common.exception.BizExceptionEnum;
+import com.sigo.oa.core.base.controller.BaseController;
+import com.sigo.oa.core.template.engine.base.GunsTemplateEngine;
+import com.sigo.oa.core.util.ToolUtil;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 代码生成控制器
+ *
+ * @author guochang
+ * @Date 2017-05-23 18:52:34
+ */
+@Controller
+@RequestMapping("/code")
+public class CodeController extends BaseController {
+
+    private String PREFIX = "/system/code/";
+
+    /**
+     * 跳转到代码生成首页
+     */
+    @RequestMapping("")
+    public String index() {
+        return PREFIX + "code.html";
+    }
+
+    /**
+     * 代码生成
+     */
+    @ApiOperation("生成代码")
+    @RequestMapping(value = "/generate", method = RequestMethod.POST)
+    @ResponseBody
+    @Permission(Const.ADMIN_NAME)
+    public Object add(@ApiParam(value = "模块名称",required = true) @RequestParam  String moduleName,
+                      @RequestParam String bizChName,
+                      @RequestParam String bizEnName,
+                      @RequestParam String path) {
+        if (ToolUtil.isOneEmpty(bizChName, bizEnName)) {
+            throw new BussinessException(BizExceptionEnum.REQUEST_NULL);
+        }
+        ContextConfig contextConfig = new ContextConfig();
+        contextConfig.setBizChName(bizChName);
+        contextConfig.setBizEnName(bizEnName);
+        contextConfig.setModuleName(moduleName);
+        if (ToolUtil.isNotEmpty(path)) {
+            contextConfig.setProjectPath(path);
+        }
+
+        GunsTemplateEngine gunsTemplateEngine = new SimpleTemplateEngine();
+        gunsTemplateEngine.setContextConfig(contextConfig);
+        gunsTemplateEngine.start();
+
+        return super.SUCCESS_TIP;
+    }
+}
