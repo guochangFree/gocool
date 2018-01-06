e49916f00e6a5cf4c9a02218afa5d620e61d0d17
guochang <guochang@sigo.cn>
2018-01-06 20:34:44 +0800

    项目基础环境搭建 郭昶

---
+++ sigo-admin/src/main/java/com/sigo/oa/common/persistence/dao/TestMapper.java
@@ -0,0 +1,16 @@
+package com.sigo.oa.common.persistence.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.sigo.oa.common.persistence.model.Test;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author guochang
+ * @since 2017-07-11
+ */
+public interface TestMapper extends BaseMapper<Test> {
+
+}
---
+++ sigo-admin/src/main/java/com/sigo/oa/common/persistence/model/Test.java
@@ -0,0 +1,51 @@
+package com.sigo.oa.common.persistence.model;
+
+import com.baomidou.mybatisplus.activerecord.Model;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author guochang
+ * @since 2017-07-11
+ */
+public class Test extends Model<Test> {
+
+    private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String value;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	protected Serializable pkVal() {
+		return this.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Test{" +
+			"id=" + id +
+			", value=" + value +
+			"}";
+	}
+}
